0a3dfa16cab16bbcbd603635272f165877b30e3c
chinthanipiumi2001 <dissanayakepiumi75@gmail.com>
2024-06-27 12:32:54 +0530

    add js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Switch Left, Middle and Right Par of Banner
+function switchBanner(name){
+    const banner = document.querySelector('#banner');
+    if(!banner.classList.contains(name)){
+        banner.className='banner';
+        banner.classList.add(name);
+    }
+}
