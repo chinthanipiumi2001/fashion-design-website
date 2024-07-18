d18f64d85eb46809d119e1a1235bec2917f0dde4
chinthanipiumi2001 <dissanayakepiumi75@gmail.com>
2024-07-18 23:56:12 +0530

    add product filters

---
+++ js/isotope.js
@@ -0,0 +1,29 @@
+/**
+ * Easy selector helper function
+ * help select dom elements quicker
+ */
+const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+        return [...document.querySelectorAll(el)];
+    } else {
+        return document.querySelector(el);
+    }
+};
+
+/**
+ * Easy event listener function
+ * make it easier to add event listner
+ */
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener));
+        }else{
+            selectEl.addEventListener(type, listener);
+        }
+    }
+};
+
+ 
